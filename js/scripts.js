2bd50125362f264a9e8a8e0b720b52853fa0b9ee
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-26 10:22:09 -0700

    add a confirm instead of prompt for users age and previous work kept-commented

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you? "));
-
-  if (age >= 21) {
-    $('#drinks').show();
-  } else if (age === 21) {
-    alert("Lucky you! Now don't go crazy!");
+  var over21 = confirm("Are you over 21? Click ok for yes or Cancel for no.");
+  
+  if (over21) {
     $('#drinks').show();
   } else {
     $('#under-21').show();
   }
-
+  });
   
-});
+  
+  // var age = parseInt(prompt("How old are you? "));
+
+  // if (age > 21) {
+  //   $('#drinks').show();
+  // } else if (age === 21) {
+  //   alert("Lucky you! Now don't go crazy!");
+  //   $('#drinks').show();
+  // } else {
+  //   $('#under-21').show();
+  // }
+// });
