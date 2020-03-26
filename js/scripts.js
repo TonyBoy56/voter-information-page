f79777a614d6d974b3df96298f67b626d233898f
Taylor Phillips <taylorphillips133@gmail.com>
2020-03-26 09:22:07 -0700

    add conditional statement to js.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 
   if (age >= 21) {
     $('#drinks').show();
+  } else if (age === 21) {
+    alert("Lucky you! Now don't go crazy!");
+    $('#drinks').show();
   } else {
     $('#under-21').show();
   }
